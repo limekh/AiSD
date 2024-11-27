3d8e3fb86c70f6153400b4f0d011fc14e509728f
Milekhin <sdgright@yandex.ru>
2024-11-27 18:09:04 +0400

    add get_head(tail) and sum_list, edit operator<< and main()

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 
 template <typename T>
+struct Node {
+	T data;
+	Node* next;
+	Node* prev;
+
+	Node(const T& value) : data(value), next(nullptr), prev(nullptr) {}
+};
+
+template <typename T>
 class LinkedList {
-
-	template <typename T>
-	struct Node {
-		T data;
-		Node* next;
-		Node* prev;
-
-		Node(const T& value) : data(value), next(nullptr), prev(nullptr) {}
-	};
-
-	Node <T>* head;
-	Node <T>* tail;
-
+	Node<T>* head;
+	Node<T>* tail;
 public:
 	LinkedList() : head(nullptr), tail(nullptr) {}
 	LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr) {
@@ -28,7 +26,7 @@ public:
 	LinkedList(size_t size, unsigned seed) : head(nullptr), tail(nullptr) {
 		std::srand(seed);
 		for (size_t i = 0; i < size; ++i) {
-			push_tail(std::rand() % 1000);
+			push_tail(std::rand() % 10);
 		}
 	}
 
@@ -36,6 +34,13 @@ public:
 		while (head) {
 			pop_head();
 		}
+	}
+
+	Node<T>* get_head() const{
+		return head;
+	}
+	Node<T>* get_tail() const{
+		return tail;
 	}
 
 	void push_tail(const T& value) {
@@ -60,6 +65,7 @@ public:
 	}
 
 	void push_head(const T& value) {
+		if (value == 0) return;
 		Node<T>* newNode = new Node<T>(value);
 		if (!head) {
 			head = tail = newNode;
@@ -140,34 +146,59 @@ public:
 	template <typename T>
 	friend std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list) {
 		Node<T>* p = list.head;
-		os << "[ ";
 		while (p) {
 			os << p->data << " ";
 			p = p->next;
 		}
-		os << "]\n";
+		os << "\n";
 		return os;
 	}
 };
 
+template <typename T>
+LinkedList<T> sum_list(const LinkedList<T>& list, const LinkedList<T>& other) {
+	LinkedList<T> result;
+	Node<T>* p1 = list.get_tail();
+	Node<T>* p2 = other.get_tail();
 
+	int carry = 0;
+	while (p1 || p2 || carry) {
+		int sum = carry;
+		if (p1) {
+			sum += p1->data;
+			p1 = p1->prev;
+		}
+		if (p2) {
+			sum += p2->data;
+			p2 = p2->prev;
+		}
+		result.push_head(sum % 10);
+		carry = sum / 10;
+	}
+	return result;
+}
 
 int main() {
 
-	LinkedList<int> list = LinkedList<int>(5, 1);
-	std::cout << list;
+	LinkedList<int> list = LinkedList<int>(3, 1);
+	LinkedList<int> copy = list;
+	std::cout << "list: " << list;
+	std::cout << "copy: " << copy;
 
-	list.push_tail(334);
-	list.push_tail(123);
-	list.push_head(456);
-	std::cout << list;
+	list.push_tail(3);
+	list.push_tail(1);
+	list.push_head(4);
+	list.push_head(0);
+	std::cout << "push_head and push_list: " << list;
 
 	list.pop_head();
 	list.pop_tail();
-	std::cout << list;
+	std::cout << "pop_head and pop_list: " << list;
 
-	list.delete_node(334);
-	std::cout << list;
+	list.delete_node(3);
+	std::cout << "delete_node: " << list;
+
+	std::cout << "SUM equals: " << sum_list(list, copy);
 
 	return 0;
 }
